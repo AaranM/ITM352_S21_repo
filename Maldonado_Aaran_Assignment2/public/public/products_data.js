10efad1db9ceb0f92efbee4fff01669d4105bcf5
AaranM <77362695+AaranM@users.noreply.github.com>
2021-04-23 01:47:06 -1000

    Add files via upload

---
+++ Maldonado_Aaran_Assignment2/public/public/products_data.js
@@ -0,0 +1,26 @@
+products = [
+    {"name" : "Saxophone" ,
+    "price" : 25.00 ,
+    "image" : "./images/altosaxwcase.jpg" ,
+    "desc"  : "This is a Alto Saxophone" },
+
+    {"name" : "Clarinet" ,
+    "price" : 20.00 ,
+    "image" : "./images/clarinetwcase3.jpg" ,
+    "desc"  : "This is a B♭ Clarinet" },
+
+    {"name" : "Trumpet" ,
+    "price" : 30.00 ,
+    "image" : "./images/trumpet.jpg" ,
+    "desc"  : "This is a Trumpet" },
+
+    {"name" : "Trombone" ,
+    "price" : 40.00 ,
+    "image" : "./images/trombonewcase.jpg" ,
+    "desc"  : "This is a Trombone" },
+
+    {"name" : "Piano" ,
+    "price" : 355.00 ,
+    "image" : "./images/piano.jpg" ,
+    "desc"  : "This is a Grand Piano" }
+   ];
